b68d42af6b38616cb3524744d44221c899e7981f
Efitric <shcoory2505@outlook.com>
2025-01-22 17:35:42 +0200

    change tha algo to reduce and fixed a bug in the front

--- backend-better-better/src/service/idService.ts
+++ backend-better-better/src/service/idService.ts
@@ -6,22 +6,13 @@ export class IdService {
 
   async createService(identityDocument: string): Promise<IdentityNumber> {
     try {
-      let sum: number = 0;
       let multiplayer: number = 2;
-      let num: number = parseInt(identityDocument);
-
-      if (isNaN(num)) {
-        throw new Error();
-      }
-
-      while (num != 0) {
-        const digit: number = num % 10;
-
-        sum += (digit * multiplayer % 10) + Math.floor(digit * multiplayer / 10);
-
-        multiplayer = multiplayer === 1 ? multiplayer = 2 : multiplayer = 1;
-        num = Math.floor(num / 10);
-      }
+      const array: string[] = identityDocument.split('');
+      
+      const sum:number = array.reduce( (currentValue,digit) =>{
+      multiplayer = multiplayer === 1 ? multiplayer = 2 : multiplayer = 1;
+      return currentValue + ( parseInt(digit) * multiplayer % 10) + Math.floor(parseInt(digit) * multiplayer / 10)}
+      ,0);
 
       const identityNumber: IdentityNumber = {
         identityDocument,
